a3cc8b903252b31439093b8c7f08b7b29f5360e4
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2020-01-27 07:46:32 -0500

    randombutton

---
+++ examples/CC-GettingStarted/Assets/Scripts/randomnumber.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class randomnumber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text outputText;
+    public Button genRandom;
+    int randNum = 0;
+    void Start()
+    {
+       genRandom.onClick.AddListener(newRandomNumber); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void newRandomNumber(){
+        randNum = Random.Range(0,255);
+        outputText.text = randNum.ToString();
+    }
+}
