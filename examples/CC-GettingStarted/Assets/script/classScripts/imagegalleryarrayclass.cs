193ab5cf64a87456609ea44f2bcfc2fcb12f1f27
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2020-02-26 13:10:09 -0500

    ispy

--- examples/CC-GettingStarted/Assets/script/classScripts/imagegalleryarrayclass.cs
+++ examples/CC-GettingStarted/Assets/script/classScripts/imagegalleryarrayclass.cs
@@ -18,6 +18,19 @@ public class imagegalleryarrayclass : MonoBehaviour
     void Start()
     {
         
+        imageArray[0].enabled = false;
+        imageArray[1].enabled = false;
+        imageArray[2].enabled = false;
+        imageArray[3].enabled = false;
+        //For loop as 3 parts: 
+        //Part 1: Start Variable
+        //part 2: How Long/Condition to make our loop stop 
+        //part 3: Itterator, how do we want to count, i++ => i=i+1;
+        for(int i = 0; i<imageArray.Length; i++){
+            imageArray[i].enabled = false;
+        }
+
+        imageArray[0].enabled = true;
 
         next.onClick.AddListener(()=>{
 
@@ -34,6 +47,13 @@ public class imagegalleryarrayclass : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        for(int j = 0; j < imageArray.Length; j++){
+            if(j == counter){
+                imageArray[j].enabled = true;
+            }else{
+                imageArray[j].enabled = false;
+            }
+        }
         
     }
 
---
+++ examples/CC-GettingStarted/Assets/script/classScripts/triviaScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class triviaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text questionOutput;
+    public Text correctIncorrect;
+    public Button[] answerButtons = new Button[4];
+    //public Button[] answerButtons;
+    string[] animals = {"Elephant","Hedgehog", "Chimpanzee", "Chameleon"};
+
+    string currentAnswer = "";
+
+    string[] questions = {"Which animal is born blind?", "A group of this animal is called an array?", "Which animal is our closest relative?","Which animal can see UV light?"};
+    string[] answerKey = {"Elephant", "Hedgehog","Chimpanzee", "Chameleon"};
+    int currentQuestion = 0;//keeping track of the current question
+
+    void Start()
+    {
+        //answerButtons[0].image.color = new Color32(0,255,0,127);
+        //answerButtons[0].GetComponentInChildren<Text>().text = animals[0];
+        //answerButtons[0].GetComponentInChildren<Text>().color = new Color32(255,0,0,255);
+
+        for(int i = 0; i < answerButtons.Length; i++){
+            answerButtons[i].image.color = new Color32(0,255,0,127);
+            answerButtons[i].GetComponentInChildren<Text>().text = animals[i];
+            answerButtons[i].GetComponentInChildren<Text>().color = new Color32(255,0,0,255);
+        
+        }
+
+        answerButtons[0].onClick.AddListener(()=>{
+            Debug.Log(animals[0]);
+            currentAnswer = animals[0];
+
+        });
+
+        answerButtons[1].onClick.AddListener(()=>{
+            Debug.Log(animals[1]);
+            currentAnswer = animals[1];
+
+        });
+
+        answerButtons[2].onClick.AddListener(()=>{
+            Debug.Log(animals[2]);
+            currentAnswer = animals[2];
+
+        });
+
+        answerButtons[3].onClick.AddListener(()=>{
+            Debug.Log(animals[3]);
+            currentAnswer = animals[3];
+
+        });
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       questionOutput.text = questions[currentQuestion];
+       if(currentAnswer != ""){
+           if(currentAnswer == answerKey[currentQuestion]){
+               Debug.Log("Correct");
+           }else{
+               Debug.Log("Incorrect");
+           }
+
+           currentAnswer = "";
+       } 
+    }
+}
